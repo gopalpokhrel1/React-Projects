a81c1cd301074fcaedaffa055007f4af65b296fa
Gopal Pokhrel <gopalpokhrel101@gmail.com>
2023-09-09 21:39:11 +0545

    Project 2

---
+++ project_2/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Navigation from './Components/Navigation'
+
+function App() {
+
+
+  return (
+    <>
+    <Navigation/>
+    </>
+  )
+}
+
+export default App
---
+++ project_2/src/Components/Navigation.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function Navigation() {
+  return (
+    <>
+     <div className="navigation">
+        <div className="logo">
+            <h1>Logo</h1>
+        </div>
+        <div className="list">
+            <ul>
+                <li>Home</li>
+                <li>About </li>
+                <li>Blog </li>
+                <li>Contact Us</li>
+            </ul>
+        </div>
+     </div>
+    </>
+  )
+}
